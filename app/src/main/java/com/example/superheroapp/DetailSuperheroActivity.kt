4eb62f76601126c49990e6ad42f0a9b402718047
CarlosMurillo <carlmuva@hotmail.com>
2023-04-07 23:55:26 -0500

    se modifico boton de regresar

--- app/src/main/java/com/example/superheroapp/DetailSuperheroActivity.kt
+++ app/src/main/java/com/example/superheroapp/DetailSuperheroActivity.kt
@@ -1,5 +1,6 @@
 package com.example.superheroapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,41 +18,41 @@ import kotlin.math.roundToInt
 
 class DetailSuperheroActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val EXTRA_ID = "extra_id"
     }
 
-    private lateinit var binding:ActivityDetailSuperheroBinding
-    private var mActivity: MainActivity? =null
-
+    private lateinit var binding: ActivityDetailSuperheroBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailSuperheroBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val id:String = intent.getStringExtra(EXTRA_ID).orEmpty()
+        val id: String = intent.getStringExtra(EXTRA_ID).orEmpty()
         getSuperheroInformation(id)
 
         val mReturnBtn = binding.btnReturn
+        mReturnBtn.setOnClickListener { navigateToSuperheroMain() }
 
-        mReturnBtn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-
-
+        /* mReturnBtn.setOnClickListener {
+             startActivity(Intent(this, MainActivity::class.java))
+         }*/
     }
 
 
-
+    private fun navigateToSuperheroMain() {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
 
 
     private fun getSuperheroInformation(id: String) {
         CoroutineScope(Dispatchers.IO).launch {
-        val superheroDetail = getRetrofit().create(ApiService::class.java).getSuperheroDetail(id)
-
-            if(superheroDetail.body() != null){
-                runOnUiThread{ createUI(superheroDetail.body()!!)}
+            val superheroDetail =
+                getRetrofit().create(ApiService::class.java).getSuperheroDetail(id)
+            if (superheroDetail.body() != null) {
+                runOnUiThread { createUI(superheroDetail.body()!!) }
             }
         }
 
@@ -61,13 +62,10 @@ class DetailSuperheroActivity : AppCompatActivity() {
     private fun createUI(superhero: SuperHeroDetailResponse) {
 
         Picasso.get().load(superhero.image.url).into(binding.ivSuperhero)
-        binding.tvSuperheroName.text=superhero.name
+        binding.tvSuperheroName.text = superhero.name
         prepareStats(superhero.powerstats)
-        binding.tvSuperheroRealName.text=superhero.biography.fullName
+        binding.tvSuperheroRealName.text = superhero.biography.fullName
         binding.tvPublisher.text = superhero.biography.publisher
-
-
-
 
 
     }
@@ -78,24 +76,25 @@ class DetailSuperheroActivity : AppCompatActivity() {
         params.height = powerstats.combat.toInt()
         binding.viewCombat.layoutParams=params*/
 
-        updateHeight(binding.viewCombat,powerstats.combat)
-        updateHeight(binding.viewDurability,powerstats.durability)
-        updateHeight(binding.viewSpeed,powerstats.speed)
-        updateHeight(binding.viewStrength,powerstats.strength)
-        updateHeight(binding.viewIntelligence,powerstats.intelligence)
-        updateHeight(binding.viewPower,powerstats.power)
+        updateHeight(binding.viewCombat, powerstats.combat)
+        updateHeight(binding.viewDurability, powerstats.durability)
+        updateHeight(binding.viewSpeed, powerstats.speed)
+        updateHeight(binding.viewStrength, powerstats.strength)
+        updateHeight(binding.viewIntelligence, powerstats.intelligence)
+        updateHeight(binding.viewPower, powerstats.power)
 
     }
 
-    private fun updateHeight(view: View, stat:String){
+    private fun updateHeight(view: View, stat: String) {
         val params = view.layoutParams
         params.height = pxToDp(stat.toFloat())
         view.layoutParams = params
 
     }
 
-    private fun pxToDp(px:Float):Int{
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,px,resources.displayMetrics).roundToInt()
+    private fun pxToDp(px: Float): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, resources.displayMetrics)
+            .roundToInt()
 
     }
 
@@ -108,7 +107,6 @@ class DetailSuperheroActivity : AppCompatActivity() {
 
 
     }
-
 
 
 }
